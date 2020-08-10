c644c3895e56ae27cd2e89e2cb724ad55fc56ed8
Gerson Jaber <jaber@mastercase.com.br>
2020-08-10 17:49:22 -0400

    Version 1.0.2 update

--- index.js
+++ index.js
@@ -41,35 +41,13 @@ ipcMain.on('app_version', (event) => {
 });
 
 autoUpdater.on('update-available', () => {
-    mainWindow.webContents.send('update_available');
+  mainWindow.webContents.send('update_available');
 });
 
 autoUpdater.on('update-downloaded', () => {
-mainWindow.webContents.send('update_downloaded');
+  mainWindow.webContents.send('update_downloaded');
 });
-
-const notification = document.getElementById('notification');
-const message = document.getElementById('message');
-const restartButton = document.getElementById('restart-button');
-ipcRenderer.on('update_available', () => {
-  ipcRenderer.removeAllListeners('update_available');
-  message.innerText = 'A new update is available. Downloading now...';
-  notification.classList.remove('hidden');
-});
-ipcRenderer.on('update_downloaded', () => {
-  ipcRenderer.removeAllListeners('update_downloaded');
-  message.innerText = 'Update Downloaded. It will be installed on restart. Restart now?';
-  restartButton.classList.remove('hidden');
-  notification.classList.remove('hidden');
-});
-
-function closeNotification() {
-    notification.classList.add('hidden');
-}
-  function restartApp() {
-    ipcRenderer.send('restart_app');
-}
 
 ipcMain.on('restart_app', () => {
-    autoUpdater.quitAndInstall();
+  autoUpdater.quitAndInstall();
 });
